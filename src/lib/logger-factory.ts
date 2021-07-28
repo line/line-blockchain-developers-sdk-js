24ed4dc5d74bf5e789adf7a251eb2de4617fe1b7
yoonyoul <yoonyoul@linecorp.com>
2021-07-28 16:47:44 +0900

    fix: update default log-level to info

--- src/lib/logger-factory.ts
+++ src/lib/logger-factory.ts
@@ -3,7 +3,8 @@ import _ from "lodash";
 
 export class LoggerFactory {
   static logger(name: string, config: object = {}): Logger {
-    const loggerConfig = config ? _.cloneDeep(config) : { "name": name, "exposeErrorCodeFrame": false, "displayFilePath": "hidden" };
+
+    const loggerConfig = config ? _.cloneDeep(config) : { "name": name, "exposeErrorCodeFrame": false, "displayFilePath": "hidden", "minLevel": "info" };
     loggerConfig["name"] = name;
     return new Logger(loggerConfig);
   }
