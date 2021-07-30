e4f1a12398f888c69fb3eaed6c1966369432a6fa
yoonyoul <yoonyoul@linecorp.com>
2021-07-30 16:36:03 +0900

    fix: remove sourceMap option and ignore dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './http-client-base';
-export * from './request';
-export * from './response';
-export * from './signature-generator';
-export * from './request-body-flattener';
--- dist/request-body-flattener.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class RequestBodyFlattener {
-    static flatten(requestBody?: {}): string;
-}
